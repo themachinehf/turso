a8b1bf51b4c6cbdf487b1a8e9aa33413de365065
Mikaël Francoeur <mikael@turso.tech>
2026-03-23 16:31:21 -0400

    use separate AST node type

--- parser/src/ast/fmt.rs
+++ parser/src/ast/fmt.rs
@@ -1714,7 +1714,10 @@ impl ToTokens for ColumnConstraint {
                 expr.to_tokens(s, context)?;
                 s.append(TK_RP, None)?;
                 if let Some(typ) = typ {
-                    typ.to_tokens(s, context)?;
+                    match typ {
+                        GeneratedColumnType::Virtual => s.append(TK_VIRTUAL, None)?,
+                        GeneratedColumnType::Stored => s.append(TK_ID, Some("STORED"))?,
+                    }
                 }
                 Ok(())
             }
